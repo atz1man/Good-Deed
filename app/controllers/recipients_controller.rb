eef4481500bd19e2127b04c3c0fb4c76b5f87492
Ahmed.O <ao.osman14@outlook.com>
2022-09-06 15:48:04 +0100

    updating

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,25 +1,29 @@
 class RecipientsController < ApplicationController
 
-def new
-  @recipent = Recipient.new
-end
+  def new
+    @recipent = Recipient.new
+  end
 
-def create
-  @recipient = Recipient(recipient_params)
-  @recipient.user = current_user
-  # Logic for redirecting the recipent
-  # if @recipient.save!
-  #   redirect
-  # else
-end
+  def index
+
+  end
+
+  def create
+    @recipient = Recipient(recipient_params)
+    @recipient.user = current_user
+    # Logic for redirecting the recipent
+    # if @recipient.save!
+    #   redirect
+    # else
+  end
 
 
-def show
-  @recipient = Recipient.find(params[:id])
-  @donation = Donation.new(recipient: @recipient)
-end
+  def show
+    @recipient = Recipient.find(params[:id])
+    @donation = Donation.new(recipient: @recipient)
+  end
 
-def index
-@recipient = Recipient.all
-end
+  def index
+    @recipient = Recipient.all
+  end
 end
