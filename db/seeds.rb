fa8d99394da7cdb29ed3a17cb7824a72fde2e53f
Ahmed.O <ao.osman14@outlook.com>
2022-09-09 13:02:28 +0100

    seed have sorted with the qr generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
+require "open-uri"
 
 
 puts "cleaning database"
@@ -48,16 +49,76 @@ puts "------------------"
 
 puts "creating recipient profiles"
 
-3.times do
-   Recipient.create(
+# 1 Recipieint
+1.times do
+  recipient = Recipient.create(
     user: admin,
     name: Faker::FunnyName.name,
-    bio: Faker::Book.title,
-    profile_photo: Faker::LoremPixel.image,
-    qr_code: Faker::Barcode.ean
+    bio: "I recently became divorced, after I became divorced also lost my job 😔 ",
+
   )
+  recipient.save!
+  qrcode = RQRCode::QRCode.new("https://good-deed1.herokuapp.com/recipients/#{recipient.id}")
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 1,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 10,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 250
+    )
+
+    Cloudinary::Uploader.upload("#{png.to_data_url}",
+      :public_id => "qr_code#{recipient.id}",
+      :use_filename => true
+      )
+
+  file = URI.open("https://images.unsplash.com/photo-1542367787-4baf35f3037d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80")
+recipient.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+recipient.save!
+
   puts "recipient profile created"
 end
+
+# 2 Second Recipient
+1.times do
+  recipient = Recipient.create(
+    user: admin,
+    name: Faker::FunnyName.name,
+    bio: "The cost living has been increasing and could not afford rent 💸",
+
+  )
+  recipient.save!
+  qrcode = RQRCode::QRCode.new("https://good-deed1.herokuapp.com/recipients/#{recipient.id}")
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 1,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 10,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 250
+    )
+
+    Cloudinary::Uploader.upload("#{png.to_data_url}",
+      :public_id => "qr_code#{recipient.id}",
+      :use_filename => true
+      )
+
+  file = URI.open("https://images.unsplash.com/photo-1529520161780-2573a0926b31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80")
+recipient.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+recipient.save!
+
+  puts "recipient profile created"
+end
+
 
 puts "------------------"
 puts "recipient profiles have been created"
@@ -72,6 +133,61 @@ puts "creating donations"
   )
   puts "donation created"
 end
+
+# 3 Second Recipient
+1.times do
+  recipient = Recipient.create(
+    user: admin,
+    name: Faker::FunnyName.name,
+    bio: "Lost my dad and the death has really affected my mental health and things became incredibly terrible 😢 ",
+
+  )
+  recipient.save!
+  qrcode = RQRCode::QRCode.new("https://good-deed1.herokuapp.com/recipients/#{recipient.id}")
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 1,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 10,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 250
+    )
+
+    Cloudinary::Uploader.upload("#{png.to_data_url}",
+      :public_id => "qr_code#{recipient.id}",
+      :use_filename => true
+      )
+
+  file = URI.open("https://images.unsplash.com/photo-1525898416124-a6464ad4dd84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80")
+recipient.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+recipient.save!
+
+  puts "recipient profile created"
+end
+
+
+puts "------------------"
+puts "recipient profiles have been created"
+puts "------------------"
+
+puts "creating donations"
+5.times do
+  Donation.create(
+    amount: rand(3..10),
+    user: user1,
+    recipient: Recipient.first
+  )
+  puts "donation created"
+end
+
+
+
+
+
 
 
 
