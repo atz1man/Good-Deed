166f37fdfb9193452184385bbaf043b00ede948a
rizwan gulamhussein <rizwan.g@live.com>
2022-09-12 14:35:32 +0100

    donation controller

--- app/javascript/controllers/amount_controller.js
+++ app/javascript/controllers/amount_controller.js
@@ -2,17 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="amount"
 export default class extends Controller {
-static targets = ["input"]
-  connect() {
-  }
-  insertamount(e){
-    // amount = parseInt(e.target.innerText.split("").slice(1).join(""))
+
+  donate(e){
+    let amount = parseInt(e.target.innerText.split("").slice(1).join(""))
     // console.log(e.target.innerText.split("").slice(1).join(""));
     // console.log(parseInt(e.target.innerText.split("").slice(1).join("")));
     // this.inputTarget.innerText = amount
-    const amount = parseInt(e.target.innerText.split("").slice(1).join(""));
-raise
-    document.querySelector('#donation_amount').value = amount;
 
+    // const amount = parseInt(e.target.innerText.split("").slice(1).join(""));
+    document.querySelector('#donation_amount').value = amount;
+    document.getElementById('#new_donation').submit();
   }
 }
