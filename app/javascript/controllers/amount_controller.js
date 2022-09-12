2ca271af26020c6568e23b6ee0eafae0859a94c1
rizwan gulamhussein <rizwan.g@live.com>
2022-09-12 12:59:08 +0100

    new features pull

---
+++ app/javascript/controllers/amount_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="amount"
+export default class extends Controller {
+static targets = ["input"]
+  connect() {
+  }
+  insertamount(e){
+    // amount = parseInt(e.target.innerText.split("").slice(1).join(""))
+    // console.log(e.target.innerText.split("").slice(1).join(""));
+    // console.log(parseInt(e.target.innerText.split("").slice(1).join("")));
+    // this.inputTarget.innerText = amount
+    const amount = parseInt(e.target.innerText.split("").slice(1).join(""));
+
+    document.querySelector('#donation_amount').value = amount;
+
+  }
+}
