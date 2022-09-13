8d4341381608559c16a7be325b11768840fa94da
Ahmed.O <ao.osman14@outlook.com>
2022-09-13 14:59:43 +0100

    url rqr code generation

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -33,7 +33,7 @@ class RecipientsController < ApplicationController
     #Previous version linking to the heroku app address
     #qrcode = RQRCode::QRCode.new("https://good-deed1.herokuapp.com/recipients/#{@recipient.id}")
    # www.good-deed.co
-    qrcode = RQRCode::QRCode.new("https:// www.good-deed.co/recipients/#{@recipient.id}")
+    qrcode = RQRCode::QRCode.new("https://www.good-deed.co/recipients/#{@recipient.id}")
     png = qrcode.as_png(
       bit_depth: 1,
       border_modules: 1,
