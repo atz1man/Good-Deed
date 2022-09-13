4804f3c6428d6905ff3dae9dfafcdad51d2e2117
Ahmed.O <ao.osman14@outlook.com>
2022-09-13 14:46:10 +0100

    remove seed

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -30,7 +30,10 @@ class RecipientsController < ApplicationController
     @recipient = Recipient.new(recipient_params)
     @recipient.user = current_user
     @recipient.save!
-    qrcode = RQRCode::QRCode.new("https://good-deed1.herokuapp.com/recipients/#{@recipient.id}")
+    #Previous version linking to the heroku app address
+    #qrcode = RQRCode::QRCode.new("https://good-deed1.herokuapp.com/recipients/#{@recipient.id}")
+   # www.good-deed.co
+    qrcode = RQRCode::QRCode.new("https:// www.good-deed.co/recipients/#{@recipient.id}")
     png = qrcode.as_png(
       bit_depth: 1,
       border_modules: 1,
